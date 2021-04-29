edf38b182c9462cc82275f79b55f9e67d14504f3
Bladi Duran <bladi.duran@gmail.com>
2021-04-28 21:57:45 -0400

    background image and header section prototype

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,11 +9,35 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+    integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        
+
 
         <!-- Styles -->
        
     </head>
     <body>
-      <div id="bigone">BLURB</div>
+    
+     <img id="background-img" src="/img/Flag-Wood-Background.png" >
+     
+    <div id="bigone">Need help posting an ad? <span class="click-text">Click Here.</span> </div>
+    <section id="navsection">
+    <div class="icon-hold" style="display: flex; ">
+   
+     <i class="fa fa-envelope" style="display: flex; " aria-hidden="true"> wasatchfirearmnetwork@gmail.com</i>  
+       <i class='fa fa-phone' style="display: inline-block;  ">888-272-5338</i>
+    </div>
+       
+      
+    
+    </section> 
+
+
+
+
+    <div id="bigtwo">Need help posting an ad? Click Here.</div>
+        
+     
     </body>
 </html>
