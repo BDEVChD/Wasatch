a67f013d239aea8b08e9df638eed6a5a5130dae0
Bladi Duran <bladi.duran@gmail.com>
2021-04-28 23:23:34 -0400

    added nav section with hover

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
         <link rel="stylesheet" href="/css/app.css">
 
         <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Merriweather:wght@300&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
          <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
     integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
@@ -28,10 +29,23 @@
         <i class="fa fa-envelope" style="display: flex; " aria-hidden="true"> wasatchfirearmnetwork@gmail.com</i>  
       
         <i class='fa fa-phone' style="display: inline-block;  ">888-272-5338</i>
+     
         </div>   
     </section> 
     <div id="bigtwo">
      <img style="height: 210px; width: 210px; padding-bottom: 10px; align-items: center;   " src="/img/WasatchLogosmall.png">
+     <div id="nav-links" style="padding: 10px;  " >
+        
+       
+        <a href="#"  >Home</a>
+        <a href="#">Utah Gun Classifieds</a>
+        <a href="#">Categories <i class="fa fa-caret-down"></i> </a>
+        <a href="#">Add Listing</a>
+        <a href="#">Manage Listing</a>
+        <a href="#" >Contact Us</a>
+     
+     
+     </div>
     </div>
     <section id="logo-content">
      
