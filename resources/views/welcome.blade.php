9a5900b43067cf5cfc73910f294f70e523d73ea7
Bladi Duran <bladi.duran@gmail.com>
2021-05-01 16:57:13 -0400

    added hover effects

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,11 @@
         <link rel="stylesheet" href="/css/app.css">
 
         <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@500;600&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Merriweather:wght@300&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Domine:wght@700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Orelega+One&display=swap" rel="stylesheet">
          <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
     integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         
@@ -24,17 +27,23 @@
      
     <div id="bigone">Need help posting an ad? <span class="click-text">Click Here.</span> </div>
     <section id="navsection">
-        <div class="icon-hold" style="display: flex; ">
+        <div class="icon-hold">
     
-            <i class="fa fa-envelope" style="padding: 40px; " aria-hidden="true"> wasatchfirearmnetwork@gmail.com</i>  
-        
-            <i class='fa fa-phone' style="padding: 40px;  ">888-272-5338</i>
-     
-        </div>   
+            <i class="fa fa-envelope" style=" " aria-hidden="true"> 
+             <span class="icontext" style="position:absolute; left: 35px; ">wasatchfirearmnetwork@gmail.com</span></i> 
+            
+        </div>         
+    </section> 
+       <section id="navsection" style="  background: linear-gradient(180deg, rgba(166,60,60,1) 85%, rgba(124,33,33,1) 100%);
+    // background: linear-gradient(0deg, rgba(208,141,141,1) 0%, rgba(175,58,58,1) 0%);">
+        <div class="icon-hold">
+             <i class='fa fa-phone' style="position:relative; top: -10px; " >  <span class="icontext" >888-272-5338</span></i>
+        </div> 
+          
     </section> 
     <section id="bigtwo">
-     <img style="height: 210px; width: 210px; padding-bottom: 10px; align-items: center;   " src="/img/WasatchLogosmall.png">
-        <div id="nav-links" style="padding: 10px;  " >
+     <img style="height: 280px; width: 240px; padding: 20px 0; align-items: center;   " src="/img/WasatchMedium.png">
+        <div id="nav-links"  >
             <a href="#"  >Home</a>
             <a href="#">Utah Gun Classifieds</a>
             <a href="#">Categories <i class="fa fa-caret-down"></i> </a>
@@ -43,10 +52,16 @@
             <a href="#" >Contact Us</a>
         </div>
         <section id="bigstuff">
-        Proud <br>
-        Patriotic <br>
-        Responsible <br>
-        <span id="buy">Buy, sell, and trade firearms in Utah.</span><br>
+          <span class="bigtext">Proud</span>
+        <span class="bigtext"> Patriotic </span>
+        <span class="bigtext">Responsible</span>
+       
+        <span id="buy">Buy, sell, and trade firearms in Utah.</span>
+        <div class="button">
+          <h3 class="yo" >
+        Utah Gun Classifieds </h3>
+        </div>
+      
         
         
         
@@ -60,54 +75,108 @@
                     <img src="/img/Good-Times-Square-Ad.png">
                 </div>
              </div>
-            <div class="promotional-text">
-             HI 
+            <div class="promotional-text-left">
+                <div class="content-left">
+                <span class="inside" style="color: #A63C3C; font-family: 'EB Garamond', serif; font-size: 3.4rem; margin-bottom: 20px;  ">YOU ARE ENTERED TO WIN A FREE</span>
+                <span class="inside" style="color:#363973;font-size: 2.4rem " >Good Times Targets AR 500 Target</span>
+                <span class="inside" style="color:#363973; ">Or A</span>
+                <span class="inside" style="color:#363973; font-size: 2.4rem ">Victory Operational Works Holster</span>
+                <span class="inside" style="color:#A63C3C; ">EVERY TIME YOU POST WITH US!</span>
+                <span class="inside" style="color:#363973; ">
+The winner will have a choice of the Targets or holsters offered, or can upgrade and pay the difference for other items.</span>
+
+
+ </div>
             
             </div>
     
     
         </section>
-          <section id="classifieds-text" style="background: white; ">
-            <div class="promotional-section">HI </div>
-            <div class="promotional-images" >Browse Our Products</div>
-    
-    
-            </section>
-          <section id="browse-wrapper"  >
+        <section id="classifieds-section">
+          <span class="classified-title" style="font-size: 3rem;" >Gun Classifieds in Utah</span> 
+            <p class="classified-title">Wasatch Firearm Network is a free platform for Utah gun owners and businesses to safely buy, sell, and trade their personal firearms. We are passionate about our community and we welcome you to be part of it. We are passionate about our 2nd amendment rights, and believe we can combat gun control by doing our part as responsible gun owners</p>     
+             <span style="color: #A63C3C">Our 2nd amendment is under attack! Please help us protect it by following all gun laws and only buying, selling, and trading with other proud, patriotic, responsible gun owners.</span>
+                <span class="browse" style="color:#363973; ">Browse Our Products By Category</span>
+         
+        </section>
+
+          <section id="browse-wrapper" >
             
             <div id="card" style="col-md-4;">
                 <img  src="/img/shutterstock1.png">
+                 <div >
+                    <i class="fa fa-angle-double-right" style="font-weight: 500; font-size: 2.4rem; position:relative; top: 10px;"></i>
+                    <div style="position: relative; top: 0px; display: inline-block; font-size: 1.5rem;">View All Firearms & Ammo </div>
+                     <div class="blackbox">
+                     
+                     </div>  
+                        <div class="info">Firearms & Ammo</div>
+                        <div class="classified-text"  >View Classifieds</div>
+
+                </div>     
             </div>
             <div id="card" style="col-md-4;">
                 <img  src="/img/shutterstock2.png">
+                 <div >
+                    <i class="fa fa-angle-double-right" style="font-weight: 500; font-size: 2.4rem; position:relative; top: 10px;"></i>
+                    <div style="position: relative; top: 0px; display: inline-block; font-size: 1.5rem;">View All Firearms & Ammo </div>
+                     <div class="blackbox">
+                       
+                     </div>  
+                     <div class="info">Firearms & Ammo</div>
+                        <div class="classified-text"  >View Classifieds</div>
+                </div>     
             </div>
-            <div id="card" style="col-md-4;">
+            <div id="card" style="col-md-4; ">
                 <img  src="/img/shutterstock3.png">
+                <div >
+                    <i class="fa fa-angle-double-right" style="font-weight: 500; font-size: 2.4rem; position:relative; top: 10px;"></i>
+                    <div style="position: relative; top: 0px; display: inline-block; font-size: 1.5rem;">View All Firearms & Ammo </div>
+                     <div class="blackbox">
+                        
+                     </div>  
+                     <div class="info">Firearms & Ammo</div>
+                     <div class="classified-text"  >View Classifieds</div>
+
+                </div>     
+
+                    
             </div>
+
+
+            {{-- <div style="text-align: center; justify-content: center; ">
+                Utah Gun Network | Utah Gun Trader
+                </div> --}}
         </section>
         <section id="values-wrapper" style="background: white; ">
-            <div class="promotional-section">HI </div>
+        Utah Gun Classifieds With A Purpose
+By The People, For The People.
+We Are Dedicated To Making A Difference.
+We believe in preserving the human right of self-defense.
+
+Our mission is to encourage and support private firearm ownership among respectful, law abiding, and patriotic citizens of the state of Utah.
+            <div class="promotional-section">More Than Just Classifieds. Our Values:
+PRIDE
+
+We are proud of our firearm heritage and who we are as Utah gun owners.
+
+PATRIOTISM
+
+We love our country, and are committed to preserving the constitution which guarantees individual liberty.
+
+RESPONSIBILITY
+
+The 2nd Amendment will be preserved or lost due to the actions of gun owners. We are here to empower our citizens to act responsibly to preserve it. </div>
             <div class="promotional-images">DIST</div>
     
     
         </section>
-         <section id="mission-wrapper" style="background: white; ">
-            <div class="promotional-section">HI </div>
-            <div class="promotional-images">DIST</div>
-    
-    
-        </section>
+      
               <section id="footer-wrapper">
      <div id="footer"> © 2019 Wasatch Firearm Network. <br> Utah Web Design and Utah SEO by Ranksey</div></section>
   
     </section>
-      <section id="promotional-wrapper" >YOU
-            <div class="promotional-section">HI </div>
-            <div class="promotional-images">DIST</div>
     
-    
-        </section>
- 
     
 
   
